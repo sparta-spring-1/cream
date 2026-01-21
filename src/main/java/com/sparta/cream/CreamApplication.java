69b95634993dd3ae085c59e69a2ab8c43eae1ad9
B-ChJ <bchaejoo@gmail.com>
2026-01-21 20:20:59 +0900

    add: 프로젝트 세팅
    프로젝트 기초 세팅
    - 의존성 추가(Web, lombok, Redis, MySQL, JPA, Spring Security, JWT)
    - application.yml 작성 (환경변수 적용)
    - editorconfig 적용
    
    Co-authored-by: [변채주]

---
+++ src/main/java/com/sparta/cream/CreamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreamApplication.class, args);
+  }
+
+}
