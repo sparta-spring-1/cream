f4f36030f22490ec24d416d665990f19cac5e2d0
B-ChJ <bchaejoo@gmail.com>
2026-01-22 15:50:35 +0900

    refactor: BaseTimeEntity 설정 추가
    - 테이블 이름을 DB에 맞춰(Snake 명명법으로) 컬럼명을 명시
    - 생성일시 수정할 수 없도록 처리(updatable = false)
    
    Co-authored-by: [변채주]

--- src/main/java/com/sparta/cream/domain/entity/common/BaseTimeEntity.java
+++ src/main/java/com/sparta/cream/domain/entity/common/BaseTimeEntity.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -27,9 +28,11 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+	@Column(name = "created_at", nullable = false, updatable = false)
 	@CreatedDate
 	private LocalDateTime createdAt;
 
+	@Column(name = "updated_at")
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
 }
