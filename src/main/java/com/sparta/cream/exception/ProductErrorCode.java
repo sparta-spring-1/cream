bb288b38d5415771f1899ed8b72d1b990f932c53
heo <foresty110@gmail.com>
2026-01-27 12:22:19 +0900

    update: 관리자 상품 수정 API 구현
    - product error code 추가
    - 서비스 로직에서 엔티티 필드 조회를 위해 image,option,category엔티티 @Getter 추가
    
    Co-authored-by: 허아림
    Refs: #25

--- src/main/java/com/sparta/cream/exception/ProductErrorCode.java
+++ src/main/java/com/sparta/cream/exception/ProductErrorCode.java
@@ -10,8 +10,9 @@ import lombok.RequiredArgsConstructor;
 public enum ProductErrorCode implements BaseCode {
 
 	PRODUCT_MODELNUMBER_CONFLICT(HttpStatus.CONFLICT,"이미 존재하는 모델번호입니다."),
-	PRODUCT_NOTFOUND_CATEGORY(HttpStatus.NOT_FOUND,"존재하지 않는 카테고리입니다.");
-
+	PRODUCT_NOT_FOUND_ID(HttpStatus.NOT_FOUND,"존재하지 않는 상품입니다."),
+	PRODUCT_NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND,"존재하지 않는 카테고리입니다."),
+	PRODUCT_IMAGE_SIZE_LIMIT(HttpStatus.BAD_REQUEST,"이미지는 최대 10개까지 저장할 수 있습니다.");
 	private final HttpStatus status;
 	private final String message;
 }
