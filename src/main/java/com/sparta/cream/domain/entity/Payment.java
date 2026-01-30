4dccea0df13f34f2b853af5b4c3220a2b5fd1151
B-ChJ <bchaejoo@gmail.com>
2026-01-30 14:47:40 +0900

    fix: changeStatus 반환값 활용
    - 결제 기록 이중으로 생성되는 구조
    - changeStatus 반환값을 활용하는 구조로 수정
    
    Co-authored-by: [변채주]
    Refs: #10

--- src/main/java/com/sparta/cream/domain/entity/Payment.java
+++ src/main/java/com/sparta/cream/domain/entity/Payment.java
@@ -160,11 +160,11 @@ public class Payment extends BaseEntity {
     }
 
     public PaymentHistory completePayment(String impUid, String method, PaymentStatus status) {
-        changeStatus(status, PaymentStatus.PAID_SUCCESS);
+        PaymentHistory changed = changeStatus(status, PaymentStatus.PAID_SUCCESS);
         this.impUid = impUid;
 		this.method = method;
         this.paidAt = LocalDateTime.now();
 
-		return new PaymentHistory(status, PaymentStatus.PAID_SUCCESS, this.amount, this);
+		return changed;
     }
 }
