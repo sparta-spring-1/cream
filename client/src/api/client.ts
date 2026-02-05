bad47ec2a393f29d697ed5eebacb348163d9d931
B-ChJ <bchaejoo@gmail.com>
2026-02-05 09:33:13 +0900

    refactor: localhost에서 테스트할 수 있도록 Base URL 변경
    Co-authored-by: 변채주
    Refs: #68

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://3.38.101.128:8080';
+const API_BASE_URL = 'http://localhost:8080';
 
 const client = axios.create({
   baseURL: API_BASE_URL,
