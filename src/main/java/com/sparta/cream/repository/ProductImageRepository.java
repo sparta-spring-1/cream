20ae9d6cecd5a378d5173c9aa0ee3ba83812b235
heo <foresty110@gmail.com>
2026-02-23 19:38:23 +0900

    update: 상품 이미지 정리 조건 수정
    - 부모 상품과 연결되어 있지 않은 고아 이미지를 기준으로 정리
    
    Co-authored-by: 허아림
    Refs: #104

--- src/main/java/com/sparta/cream/repository/ProductImageRepository.java
+++ src/main/java/com/sparta/cream/repository/ProductImageRepository.java
@@ -1,12 +1,13 @@
 package com.sparta.cream.repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sparta.cream.entity.ProductImage;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-	List<ProductImage> findByDeletedAtBefore(LocalDateTime localDateTime);
+	@Query(value = "SELECT * FROM product_image WHERE product_id IS NULL", nativeQuery = true)
+	List<ProductImage> findOrphanedImages();
 }
