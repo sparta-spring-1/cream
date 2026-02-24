ac822ada41bb9b68ad8c669c337637960ad6c2cc
heo <foresty110@gmail.com>
2026-02-24 19:13:51 +0900

    fix: ProductImageRepository 중복 메서드 삭제
    - 브랜치 병합 과정에서 같은 jpa메서드가 중복 적용되었음
    
    Co-authored-by: 허아림
    Refs: #109

--- src/main/java/com/sparta/cream/repository/ProductImageRepository.java
+++ src/main/java/com/sparta/cream/repository/ProductImageRepository.java
@@ -9,9 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.sparta.cream.entity.ProductImage;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-
-	List<ProductImage> findAllByIdIn(List<Long> imageIds);
-
 	@Query(value = "SELECT * FROM product_image WHERE product_id IS NULL", nativeQuery = true)
 	List<ProductImage> findOrphanedImages();
 
